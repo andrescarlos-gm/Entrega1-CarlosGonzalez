f11b261feee6b047d683eaf63c381953441b0ff7
Carlos <andrescarlos2211@gmail.com>
2023-11-26 20:47:34 -0300

    add item count

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -2,41 +2,48 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import { Button, CardActionArea, CardActions } from "@mui/material";
+import { Button, CardActionArea, Grid } from "@mui/material";
 import { Link } from "react-router-dom";
+import ItemCount from "../ItemCount/ItemCount.jsx";
 
 export default function Item({ item }) {
+
+
   return (
     <Card sx={{ maxWidth: 345, backgroundColor: "#E5D0CC", width: 450 }}>
-      <Link to={`/item/${item.id}` } style={{ textDecoration: 'none' }}  > 
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="300"
-          image={item.ProductImage}
-          alt="image"
-        />
-                
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div" color="black">
-            {item.ProductName}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {item.ProductDescription}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
+      <Link to={`/item/${item.id}`} style={{ textDecoration: "none" }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="300"
+            image={item.ProductImage}
+            alt="image"
+          />
+
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div" color="black">
+              {item.ProductName}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {item.ProductDescription}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
       </Link>
-      <CardActions>
+      <Grid
+        container
+        spacing={1}
+        direction="column"
+        justifyContent="space-between"
+        alignItems="center"
+      >
         <Link to={`/item/${item.id}`}>
           <Button size="small" color="primary">
             Learn More
           </Button>
         </Link>
-        <Button size="medium" color="secondary" variant="contained">
-          Join the Waiting List
-        </Button>
-      </CardActions>
+        <ItemCount item={item} />
+      </Grid>
     </Card>
   );
 }
---
+++ src/Components/ItemCount/ItemCount.jsx
@@ -0,0 +1,36 @@
+import { Box, TextField, CardActions } from "@mui/material";
+import { Button, Grid } from "@mui/material";
+import { useState } from "react";
+
+export default function ItemCount({ item }) {
+  const [num, setNum] = useState(1);
+  const handleChange = (e) => {
+    const regex = /^[0-9\b]+$/;
+    if (e.target.value === "" || regex.test(e.target.value)) {
+      setNum(e.target.value);
+    }
+  };
+  console.log(num)
+
+  return (
+    <Grid item xs={8}>
+      <CardActions>
+        <Box component="form">
+          <TextField
+            type="number"
+            id="outlined-basic"
+            label="Quantity"
+            variant="outlined"
+            inputProps={{ min: 1, max: item.stock }}
+            onChange={(e) => handleChange(e)}
+                        value={num}
+            value={num}
+          />
+        </Box>
+        <Button size="medium" color="secondary" variant="contained">
+          Add to Cart
+        </Button>
+      </CardActions>
+    </Grid>
+  );
+}
