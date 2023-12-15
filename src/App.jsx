944b1d42baf7036779dfcfc3bd19088fe3644f08
Carlos González <119871541+andrescarlos-gm@users.noreply.github.com>
2023-12-15 12:37:34 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import NavBar from "./Components/NavBar/NavBar.jsx";
 import NotFound from "./Components/404/404.jsx";
 import ItemListContainer from "./Components/ItemListContainer/ItemListContainer.jsx";
-import ItemDetails from "./Components/ItemDetails/ItemDetails.jsx"
+import ItemDetail from "./Components/ItemDetails/ItemDetail.jsx"
 import { CssBaseline } from "@mui/material";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./main.css";
@@ -36,7 +36,7 @@ function App() {
         <Routes>
           <Route path="/" element={<ItemListContainer />} />
           <Route path="/category/:id" element={<ItemListContainer />} />
-          <Route path="/item/:id" element={<ItemDetails/>} />
+          <Route path="/item/:id" element={<ItemDetail/>} />
           <Route path="*" element={<NotFound />} />
           <Route path="/cart" element={<CartContainer  />} />
         </Routes>
